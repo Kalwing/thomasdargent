757c517a592762f6c493755995ed2250e1366777
Kalwing <thomasdargent@gmail.com>
2024-12-04 12:07:32 +0100

    Add an update button to the quote pages. Add quotes

--- flask_app/routes_api.py
+++ flask_app/routes_api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, url_for, request, current_app
+from flask import Blueprint, render_template, url_for, request, current_app, jsonify
 import json
 import random
 from pathlib import Path
@@ -14,17 +14,17 @@ def brevo_error():
 @bp.route("/success_brevo", methods=["GET", "POST"])
 def brevo_success():
     prefix = "brevo/"
-    return render_template(f"{prefix}brevo_success_2.html"), 200  # test: brevo/....
+    return render_template(f"{prefix}brevo_success_2.html"), 200
 
 
 # QUOTE GIVER
 def pick_quote(tag=None):
     if tag is None:
-        path = Path(current_app.static_folder)/'data/quotes.json'  # strip first / to make it a url path not a URL endpoint
+        path = Path(current_app.static_folder)/'data/quotes.json'
         with open(path, 'r') as file:
             return random.choice(json.loads(file.read()))
     else:
-        path = Path(current_app.static_folder)/'data/quotes_sorted.json'  # strip first / to make it a url path not a URL endpoint
+        path = Path(current_app.static_folder)/'data/quotes_sorted.json'
         with open(path, 'r') as file:
             quotes = json.loads(file.read())
             return random.choice(quotes[tag])
@@ -32,4 +32,8 @@ def pick_quote(tag=None):
 @bp.route("/get_quote", methods=["GET"])
 @bp.route("/get_quote/<tag>", methods=["GET"])
 def quote_giver(tag=None):
-    return pick_quote(tag)
+    response = jsonify(pick_quote(tag))
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
