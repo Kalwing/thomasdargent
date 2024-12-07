b54d16ee87e03292902c9d346afa69f3152acd4f
Kalwing <thomasdargent@gmail.com>
2024-12-07 22:06:56 +0100

    Fix issue with variable affectation in JS

--- public/js/scroll_back_footnote.js
+++ public/js/scroll_back_footnote.js
@@ -1,40 +1,42 @@
-const footnote_backs = document.querySelectorAll('.footnote-back');
-console.log('aaa', footnote_backs);
-for (var i = 0; i < footnote_backs.length; i++) {
-	console.log('a', footnote_backs[i]);
-	footnote_backs[i].addEventListener('click', process_back);
-	footnote_backs[i].addEventListener('enter', process_back);
-}
+document.addEventListener('DOMContentLoaded', () => {
+	const footnote_backs = document.querySelectorAll('.footnote-back');
+	console.log('aaa', footnote_backs);
+	for (var i = 0; i < footnote_backs.length; i++) {
+		console.log('a', footnote_backs[i]);
+		footnote_backs[i].addEventListener('click', process_back);
+		footnote_backs[i].addEventListener('enter', process_back);
+	}
 
-function process_back(evt) {
-	const back_to_id = evt.target.href.match(/#.*/)[0];
-	const back_to = document.querySelector(back_to_id);
-	console.log(back_to);
-	const viewportOffset = back_to.getBoundingClientRect();
-	const x = viewportOffset.left + window.scrollX;
-	const y = viewportOffset.top + window.scrollY;
-	const client_height = window.innerHeight;
-	console.log(x, y);
+	function process_back(evt) {
+		const back_to_id = evt.target.href.match(/#.*/)[0];
+		const back_to = document.querySelector(back_to_id);
+		console.log(back_to);
+		const viewportOffset = back_to.getBoundingClientRect();
+		const x = viewportOffset.left + window.scrollX;
+		const y = viewportOffset.top + window.scrollY;
+		const client_height = window.innerHeight;
+		console.log(x, y);
 
-	const htop = document.getElementById('highlightTop');
-	htop.style.display = 'block';
-	htop.style.height = `calc(${y}px - 2rem)`;
-	htop.style.opacity = 1;
-	const hbot = document.getElementById('highlightBot');
-	hbot.style.display = 'block';
-	hbot.style.top = `calc(${y}px + 3rem)`;
-	hbot.style.opacity = 1;
-	hbot.style.bottom = `${document.documentElement.scrollHeight}px`;
+		const htop = document.getElementById('highlightTop');
+		htop.style.display = 'block';
+		htop.style.height = `calc(${y}px - 2rem)`;
+		htop.style.opacity = 1;
+		const hbot = document.getElementById('highlightBot');
+		hbot.style.display = 'block';
+		hbot.style.top = `calc(${y}px + 3rem)`;
+		hbot.style.opacity = 1;
+		hbot.style.bottom = `${document.documentElement.scrollHeight}px`;
 
-	setTimeout(() => {
-		window.scrollTo(x, y - client_height / 3);
-	}, 50);
-	setTimeout(() => {
-		htop.style.opacity = 0;
-		hbot.style.opacity = 0;
-	}, 100);
-	setTimeout(() => {
-		htop.style.display = 'none';
-		hbot.style.display = 'none';
-	}, 1000);
-}
+		setTimeout(() => {
+			window.scrollTo(x, y - client_height / 3);
+		}, 50);
+		setTimeout(() => {
+			htop.style.opacity = 0;
+			hbot.style.opacity = 0;
+		}, 100);
+		setTimeout(() => {
+			htop.style.display = 'none';
+			hbot.style.display = 'none';
+		}, 1000);
+	}
+});
--- public/js/switch_theme.js
+++ public/js/switch_theme.js
@@ -1,16 +1,22 @@
-const lightSwitch = document.getElementById('light-switch');
-if (localStorage.getItem('dark-mode') === 'true') {
-	lightSwitch.checked = true;
-}
-lightSwitch.addEventListener('change', () => {
-	const { checked } = lightSwitch;
-	lightSwitch.checked = checked;
-
-	if (lightSwitch.checked) {
+document.addEventListener('DOMContentLoaded', () => {
+	const lightSwitch = document.getElementById('light-switch');
+	if (localStorage.getItem('dark-mode') === 'true') {
+		lightSwitch.checked = true;
 		document.documentElement.classList.add('dark');
-		localStorage.setItem('dark-mode', true);
-	} else {
-		document.documentElement.classList.remove('dark');
-		localStorage.setItem('dark-mode', false);
 	}
+	console.log(lightSwitch);
+
+	lightSwitch.addEventListener('change', () => {
+		console.log('Turn the light', lightSwitch);
+		// const { checked } = lightSwitch;
+		// lightSwitch.checked = checked;
+
+		if (lightSwitch.checked) {
+			document.documentElement.classList.add('dark');
+			localStorage.setItem('dark-mode', true);
+		} else {
+			document.documentElement.classList.remove('dark');
+			localStorage.setItem('dark-mode', false);
+		}
+	});
 });
